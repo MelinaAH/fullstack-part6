c5996ec49ea487189b879332451cd8b520d64cc1
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-03-30 10:20:49 +0300

    6.5 step3 done

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -3,7 +3,8 @@ import { addVote } from './reducers/anecdoteReducer';
 import NewAnecdote from './components/NewAnecdote';
 
 const App = () => {
-  const anecdotes = useSelector(state => state);
+  const anecdotes = useSelector(state => 
+    state.sort((a, b) => b.votes - a.votes));
   const dispatch = useDispatch();
 
   const vote = (id) => {
