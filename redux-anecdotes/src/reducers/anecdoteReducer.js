85bc2c4b41b2a3f37fabef041bdc4b8d83cc3afc
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-04-04 09:13:53 +0300

    6.14 step1 done

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,17 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const anecdotesAtStart = [
+/*const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
+];*/
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
+/*const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
@@ -19,11 +19,11 @@ const asObject = (anecdote) => {
   }
 };
 
-const initialState = anecdotesAtStart.map(asObject);
+const initialState = anecdotesAtStart.map(asObject);*/
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     addVote(state, action) {
       const id = action.payload.id;
@@ -50,9 +50,15 @@ const anecdoteSlice = createSlice({
         id: getId(),
         votes: 0,
       })
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload);
+    },
+    setAnecdotes(state, action) {
+      return action.payload;
     }
   }
-})
+});
 
-export const { addVote, createAnecdote } = anecdoteSlice.actions;
+export const { addVote, createAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
